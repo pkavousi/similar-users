bb3946528e33d7f542209d8201ececcc6b83df16
payam kavousi <Payam@payams-mbp.myfiosgateway.com>
2021-07-12 12:09:47 -0400

    added a preprocessing pipeline

---
+++ user_similarity_model/pipeline.py
@@ -0,0 +1,20 @@
+from feature_engine.encoding import OrdinalEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler
+
+from user_similarity_model.config.core import config
+
+pipe = Pipeline(
+    [
+        # ==== Categorical encoding
+        # latest_interest_tag and latest_assessment_tag
+        (
+            "Targetencoder",
+            OrdinalEncoder(
+                encoding_method="ordered",
+                variables=config.model_config.categorical_vars,
+            ),
+        ),
+        ("scaler", MinMaxScaler()),
+    ]
+)
